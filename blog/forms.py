5140b685afbef9e8e56cde9052304abb3e95497d
Jbasilio55 <54733957+Jbasilio55@users.noreply.github.com>
2024-08-03 16:00:36 -0400

    Add files via upload

---
+++ blog/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from . models import Comment, Post
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        exclude = ["post"]
+        labels = {
+            "user_name": "Your Name",
+            "user_email": "Your Email",
+            "text": "Your Comment",
+            }
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ["slug"]
+        
+        
