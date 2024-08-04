4d9dc2eeed37881a5a02f451b3540dfc23715c7b
Jbasilio55 <54733957+Jbasilio55@users.noreply.github.com>
2024-08-03 20:09:33 -0400

    Update settings.py

--- my_site/settings.py
+++ my_site/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 # STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_URL = "/static/"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static')),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Default primary key field type
